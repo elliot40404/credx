1e39a137e12dd1abd030941da88fcdcc4c512ca9
elliot404 <fortesting40404@gmail.com>
2021-02-12 20:50:21 +0530

    donee

--- app.test.js
+++ app.test.js
@@ -23,5 +23,11 @@ let users = {
 // users.cred.splice(a, 1);
 // console.log(users);
 //  ? METHOD 2
-users.cred = users.cred.filter((e) => e.id != 1);
-console.log(users);
+// users.cred = users.cred.filter((e) => e.id != 1);
+// console.log(users);
+
+const date = new Date().toISOString().split('T')[0].split('-').join('');
+const dat = Date.now()
+// const det = date.toISOString().split('T')[0];
+// console.log(det);
+console.log(dat);
